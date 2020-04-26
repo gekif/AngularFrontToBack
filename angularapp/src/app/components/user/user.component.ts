5e7830f84254064fa76a3cf383d832dcfa87e353
Gekif <dzulfikar.maulana@gmail.com>
2020-04-26 21:26:15 +0700

    3.Diving Into Components: 12.Properties _amp_ Methods

--- angularapp/src/app/components/user/user.component.ts
+++ angularapp/src/app/components/user/user.component.ts
@@ -1,17 +1,32 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css'],
-  // template: '<h2>John Doe</h2>'
-  // styles: [`
-  //   h2 {
-  //     color: blue;
-  //   }
-  // `]
 })
 
-export class UserComponent {
 
+export class UserComponent {
+  // Properties
+  firstName = 'John';
+  lastName = 'Doe';
+  age = 30;
+
+  // Method
+  constructor(){
+    // this.sayHello();
+    console.log(this.age);
+    this.hasBirtday();
+    console.log(this.age);
+  }
+
+  sayHello(){
+    console.log(`Hello ${this.firstName}`);
+  }
+
+  hasBirtday(){
+    this.age += 1;
+  }
 }

49d1c13bd4745be59a2ec4a3630a457fa41d43ec
Gekif <dzulfikar.maulana@gmail.com>
2020-04-26 22:37:58 +0700

    3.Diving Into Components: 16.Generating Components _amp_ OnInit

--- angularapp/src/app/components/user/user.component.ts
+++ angularapp/src/app/components/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { User } from '../../models/User';
 
 
@@ -9,12 +9,16 @@ import { User } from '../../models/User';
 })
 
 
-export class UserComponent {
+export class UserComponent implements OnInit {
   // Properties
   user: User;
 
   // Method
-  constructor(){
+  constructor() {
+
+  }
+
+  ngOnInit() {
     this.user = {
       firstName: 'John',
       lastName: 'Doe',

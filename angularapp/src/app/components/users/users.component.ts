d10f3d540e3104272e4cedf44e0dbad7e32c780e
Gekif <dzulfikar.maulana@gmail.com>
2020-05-02 16:50:00 +0700

    4.Template Syntax: 3.Diving Into Components: 17.Loops With ngFor

--- angularapp/src/app/components/users/users.component.ts
+++ angularapp/src/app/components/users/users.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from "../../models/User";
 
 
 @Component({
@@ -9,11 +10,58 @@ import { Component, OnInit } from '@angular/core';
 
 
 export class UsersComponent implements OnInit {
+  users: User[];
 
   constructor() { }
 
   ngOnInit() {
-    console.log('init...');
+    this.users = [
+      {
+        firstName: 'John',
+        lastName: 'Doe',
+        age: 30,
+        address: {
+          street: '50 Main St',
+          city: 'Boston',
+          state: 'MA'
+        }
+      },
+      {
+        firstName: 'Kevin',
+        lastName: 'Johnson',
+        age: 34,
+        address: {
+          street: '20 School St',
+          city: 'Lynn',
+          state: 'MA'
+        }
+      },
+      {
+        firstName: 'Karen',
+        lastName: 'Williams',
+        age: 26,
+        address: {
+          street: '55 Mill St',
+          city: 'Miami',
+          state: 'FL'
+        }
+      }
+    ];
+
+    this.addUser({
+      firstName: 'David',
+      lastName: 'Jackson',
+      age: 44,
+      address: {
+        street: '12 Wake St',
+        city: 'Miami',
+        state: 'FL'
+      }
+    });
+  }
+
+  addUser(user: User) {
+    this.users.push(user);
   }
 
 }
